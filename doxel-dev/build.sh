818d99fa78aedaaf859aacbe39e50d1651b3c12d
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2017-08-30 18:30:37 +0200

    add sample segment

--- doxel-dev/build.sh
+++ doxel-dev/build.sh
@@ -12,12 +12,12 @@ if [ -z "$NODE_VERSION" ] ; then
     echo get latest node LTS release number
     NODE_VERSION=$(nvm ls-remote | grep LTS | tail -n 1 | sed -r -n -e 's/.*(v[0-9\.]+).*/\1/p')
   else
-    NODE_VERSION=v6.9.4
+    NODE_VERSION=v6.11.1
   fi
 fi
 echo using node $NODE_VERSION
 
-sudo docker build \
+docker build \
   --build-arg debian_mirror=$DEBIAN_MIRROR \
   --build-arg nvm_version=$NVM_VERSION \
   --build-arg node_version=$NODE_VERSION \
