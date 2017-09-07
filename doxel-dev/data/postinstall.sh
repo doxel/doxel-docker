761c1a3b4906c46e2026b6da21b3e28271bc5b9a
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2017-09-07 03:15:42 +0200

    fix db update

--- doxel-dev/data/postinstall.sh
+++ doxel-dev/data/postinstall.sh
@@ -2,8 +2,5 @@
 set -e
 mv /home/doxel/doxel-loopback/server/upload_dev/2* /home/doxel/doxel-loopback/server/upload/.
 
-echo 'db.segments.update({"id": "59a6d02dd36a29013790749f"},{$set: {"status": "published"}});' > /tmp/updatedb.js
-
-mongo localhost:27017/doxel /tmp/updatedb.js
-
-
+mongo localhost:27017/doxel_dev --eval 'db.Segment.update({"_id": ObjectId("59a6d02dd36a29013790749f")},{$set: {"status": "published"}});'
+sync
