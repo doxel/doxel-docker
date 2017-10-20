5090a7ad100c62168be293b84c28bfcafefe49f5
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2017-10-20 21:24:42 +0200

    set default node version to 6.11.4, add  to docker build command

--- doxel-dev/build.sh
+++ doxel-dev/build.sh
@@ -12,12 +12,12 @@ if [ -z "$NODE_VERSION" ] ; then
     echo get latest node LTS release number
     NODE_VERSION=$(nvm ls-remote | grep LTS | tail -n 1 | sed -r -n -e 's/.*(v[0-9\.]+).*/\1/p')
   else
-    NODE_VERSION=v6.11.1
+    NODE_VERSION=v6.11.4
   fi
 fi
 echo using node $NODE_VERSION
-
 docker build \
+  $DOCKER_BUILD_OPTIONS \
   --build-arg debian_mirror=$DEBIAN_MIRROR \
   --build-arg nvm_version=$NVM_VERSION \
   --build-arg node_version=$NODE_VERSION \
